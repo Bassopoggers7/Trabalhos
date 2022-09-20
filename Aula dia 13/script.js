256c220523ef315196dac364b94507acf33a1520
David Basso <99267489+Bassopoggers7@users.noreply.github.com>
2022-09-20 12:53:34 -0300

    Create script.js

---
+++ Aula dia 13/script.js
@@ -0,0 +1,22 @@
+let velocidades = []
+
+function calculate() {
+
+    if (localStorage.getItem('velocidades') !==null) {
+        velocidades = JSON.parse(localStorage.getItem('velocidades'))
+    }
+    var dist = document.getElementById("deslocamento").value
+    var speed = document.getElementById("tempo").value
+
+    document.getElementById("velocidadeMedia").value = dist / speed
+    velocidades.push(document.getElementById("velocidadeMedia").value)
+    document.getElementById('velocidades').value = velocidades
+    localStorage.setItem('velocidades', JSON.stringify(velocidades))
+}
+
+function limpar () {
+    document.getElementById("deslocamento").value = 0
+    document.getElementById("tempo").value = 0
+    document.getElementById("velocidadeMedia").value = 0
+}
+
